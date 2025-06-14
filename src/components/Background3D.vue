9758fc22d8e20bc727915b7ce254aefe691e4422
Drew Larson <116033515+drewlarso@users.noreply.github.com>
2025-06-14 00:14:33 -0600

    add navbar and hero start

---
+++ src/components/Background3D.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import {
+    Matrix4,
+    Mesh,
+    MeshBasicMaterial,
+    MeshNormalMaterial,
+    PerspectiveCamera,
+    Scene,
+    SphereGeometry,
+    TextureLoader,
+    TorusGeometry,
+    WebGLRenderer,
+} from 'three'
+import { onMounted, render } from 'vue'
+
+onMounted(() => {
+    const scene = new Scene()
+    const camera = new PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    )
+    const renderer = new WebGLRenderer({
+        canvas: document.querySelector('#bg-three-canvas') as HTMLCanvasElement,
+    })
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+    })
+
+    const textureLoader = new TextureLoader()
+    const skyTexture = textureLoader.load('/bg.png')
+    const skySphereMaterial = new MeshBasicMaterial({ map: skyTexture })
+    const sphereGeometry = new SphereGeometry(500)
+    sphereGeometry.applyMatrix4(new Matrix4().makeScale(-1, 1, 1))
+    const skySphere = new Mesh(sphereGeometry, skySphereMaterial)
+    scene.add(skySphere)
+
+    const animate = () => {
+        renderer.render(scene, camera)
+    }
+    renderer.setAnimationLoop(animate)
+})
+</script>
+
+<template>
+    <canvas id="bg-three-canvas" class="fixed left-0 top-0"></canvas>
+</template>
---
+++ src/components/Hero.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-center h-[50vh]">
+        <!--row flex-->
+        <div class="flex flex-col justify-center w-3/4">
+            <!--column flex-->
+            <h1 class="text-6xl text-center font-extrabold leading-24">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            </h1>
+        </div>
+    </div>
+</template>
