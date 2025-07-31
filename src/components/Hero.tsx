1908e0f95919cdfee8533299adabb841121ea601
Drew Larson <116033515+drewlarso@users.noreply.github.com>
2025-07-30 20:10:04 -0600

    update hero section and phaser background elements

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,16 +2,35 @@ export default function Hero() {
     return (
         <section
             id="about"
-            className="flex flex-col items-center justify-center h-[60vh] w-full"
+            className="flex flex-col items-center justify-center min-h-dvh w-full"
         >
-            <div className="flex flex-col items-center  bg-gradient-to-br from-violet-200 to-violet-600 bg-clip-text text-transparent">
-                <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl">
+            <div className="flex flex-col items-center bg-gradient-to-br from-violet-200 to-violet-600 bg-clip-text text-transparent">
+                <h1
+                    className={`text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-center animate-bob-1`}
+                >
                     Drew Larson
                 </h1>
-                <h1 className="text-xl sm:text-2xl md:text-3xl mt-4">
-                    Software Engineer
+
+                <h1
+                    className={`text-md sm:text-xl md:text-2xl mt-6 font-medium loaded animate-bob-2 mb-6`}
+                >
+                    Software Engineering Student
                 </h1>
             </div>
+            <h1 className="text-md sm:text-lg md:text-xl text-center mt-6 font-medium min-w-[50%] w-xl">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam
+            </h1>
+            <div className="flex flex-row justify-center items-center gap-4">
+                <button className="mt-16 font-medium bg-violet-600/80 hover:bg-violet-600 transition-colors duration-200 border-2 border-violet-600/20 backdrop-blur-sm py-2 px-3 rounded-4xl">
+                    My Projects
+                </button>
+                <button className="mt-16 font-medium bg-violet-600/80 hover:bg-violet-600 transition-colors duration-200 border-2 border-violet-600/40 backdrop-blur-sm py-2 px-3 rounded-4xl">
+                    Contact Me
+                </button>
+            </div>
+            <p className="mt-16">v</p>
         </section>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,34 @@
+import { useState } from 'react'
+
 export default function Navbar() {
+    const [active, setActive] = useState<string>('About')
+
+    const links = [
+        { href: '#about', label: 'About' },
+        { href: '#skills', label: 'Skills' },
+        { href: '#projects', label: 'Projects' },
+        { href: '#contact', label: 'Contact' },
+    ]
+
     return (
-        <nav className="fixed w-full mx-auto mt-4">
+        <nav className="fixed w-full mx-auto mt-4 z-[1000]">
             <div className="flex flex-row justify-center">
-                <ul className="flex flex-row justify-center items-center backdrop-blur-lg rounded-4xl p-1 text-violet-200">
-                    <li className="p-2">
-                        <a
-                            href="#about"
-                            className="duration-100 hover:text-violet-100"
-                        >
-                            About
-                        </a>
-                    </li>
-                    <li className="p-2">
-                        <a
-                            href="#skills"
-                            className="duration-100 hover:text-violet-100"
-                        >
-                            Skills
-                        </a>
-                    </li>
-                    <li className="p-2">
-                        <a
-                            href="#projects"
-                            className="duration-100 hover:text-violet-100"
-                        >
-                            Projects
-                        </a>
-                    </li>
-                    <li className="p-2">
-                        <a
-                            href="#contact"
-                            className="duration-100 hover:text-violet-100"
-                        >
-                            Contact
-                        </a>
-                    </li>
+                <ul className="flex flex-row justify-center items-center bg-violet-400/15 border border-violet-300/20 backdrop-blur-lg rounded-4xl p-1.5 text-violet-100 font-medium">
+                    {links.map((link) => (
+                        <li className="px-2" key={link.href}>
+                            <a
+                                href={link.href}
+                                onClick={() => setActive(link.label)}
+                                className={`duration-200 hover:text-violet-50 text-sm ${
+                                    active === link.label
+                                        ? 'underline underline-offset-4 decoration-2'
+                                        : ''
+                                }`}
+                            >
+                                {link.label}
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </nav>
