bd897bba2cd97768744a5795e42a464ce8499ebd
Drew Larson <116033515+drewlarso@users.noreply.github.com>
2025-07-30 23:45:10 -0600

    update hero to fade in

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,36 +1,65 @@
+import React, { useState, useEffect } from 'react'
+
 export default function Hero() {
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        setIsLoaded(true)
+    }, [])
+
     return (
         <section
             id="about"
             className="flex flex-col items-center justify-center min-h-dvh w-full"
         >
-            <div className="flex flex-col items-center bg-gradient-to-br from-violet-200 to-violet-600 bg-clip-text text-transparent">
-                <h1
-                    className={`text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-center animate-bob-1`}
-                >
+            <div
+                className={`flex flex-col items-center bg-gradient-to-br from-violet-200 to-violet-600 bg-clip-text text-transparent transform transition-all duration-1000 ${
+                    isLoaded
+                        ? 'translate-y-0 opacity-100'
+                        : 'translate-y-10 opacity-0'
+                }`}
+            >
+                <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-center mb-6">
                     Drew Larson
                 </h1>
-
-                <h1
-                    className={`text-md sm:text-xl md:text-2xl mt-6 font-medium loaded animate-bob-2 mb-6`}
-                >
+                <h1 className="text-md sm:text-xl md:text-2xl mt-6 font-medium loaded mb-6">
                     Software Engineering Student
                 </h1>
+                <div className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-24 h-0.5 bg-gradient-to-r from-transparent via-violet-400 to-transparent"></div>
             </div>
-            <h1 className="text-md sm:text-lg md:text-xl text-center mt-6 font-medium min-w-[50%] w-xl">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam
+            <h1
+                className={`text-md sm:text-lg md:text-xl text-center text-violet-200 mt-6 font-medium min-w-[50%] w-xl transform transition-all duration-1000 delay-300 ${
+                    isLoaded
+                        ? 'translate-y-0 opacity-100'
+                        : 'translate-y-10 opacity-0'
+                }`}
+            >
+                hellohellohellohellohellohellohellohello libgy is chump
+                llohellohellohellohellohello hellohellohellohellohellohellohello
             </h1>
-            <div className="flex flex-row justify-center items-center gap-4">
-                <button className="mt-16 font-medium bg-violet-600/80 hover:bg-violet-600 transition-colors duration-200 border-2 border-violet-600/20 backdrop-blur-sm py-2 px-3 rounded-4xl">
-                    My Projects
+            <div
+                className={`flex flex-row justify-center items-center gap-4  transform transition-all duration-1000 delay-500 ${
+                    isLoaded
+                        ? 'translate-y-0 opacity-100'
+                        : 'translate-y-5 opacity-0'
+                }`}
+            >
+                <button className="mt-16 font-medium bg-gradient-to-br from-violet-700/80 to-indigo-600/80 hover:from-violet-600/80 hover:to-indigo-500/80 transition-all duration-200 border-2 border-violet-600/20 backdrop-blur-sm py-2 px-3 rounded-4xl overflow-hidden hover:scale-105">
+                    <a href="#projects">My Projects</a>
                 </button>
-                <button className="mt-16 font-medium bg-violet-600/80 hover:bg-violet-600 transition-colors duration-200 border-2 border-violet-600/40 backdrop-blur-sm py-2 px-3 rounded-4xl">
-                    Contact Me
+                <button className="mt-16 font-medium bg-gradient-to-br from-violet-700/80 to-indigo-600/80 hover:from-violet-600/80 hover:to-indigo-500/80 transition-all duration-200 border-2 border-violet-600/20 backdrop-blur-sm py-2 px-3 rounded-4xl overflow-hidden hover:scale-105">
+                    <a href="#contact">Contact Me</a>
                 </button>
             </div>
-            <p className="mt-16">v</p>
+            <p
+                className={`absolute bottom-24 font-bold text-3xl mt-16 transform transition-all duration-1000 delay-500 animate-bounce ${
+                    isLoaded
+                        ? 'translate-y-0 opacity-100'
+                        : 'translate-y-5 opacity-0'
+                }`}
+            >
+                ↓
+            </p>
         </section>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,13 +13,16 @@ export default function Navbar() {
     return (
         <nav className="fixed w-full mx-auto mt-4 z-[1000]">
             <div className="flex flex-row justify-center">
-                <ul className="flex flex-row justify-center items-center bg-violet-400/15 border border-violet-300/20 backdrop-blur-lg rounded-4xl p-1.5 text-violet-100 font-medium">
+                <ul className="flex flex-row justify-center items-center bg-violet-400/15 border border-violet-300/20 backdrop-blur-lg rounded-4xl p-1.5 text-violet-50 font-medium">
                     {links.map((link) => (
-                        <li className="px-2" key={link.href}>
+                        <li
+                            className="overflow-hidden hover:scale-102 duration-100 px-2"
+                            key={link.href}
+                        >
                             <a
                                 href={link.href}
                                 onClick={() => setActive(link.label)}
-                                className={`duration-200 hover:text-violet-50 text-sm ${
+                                className={`duration-200 hover:text-white text-sm ${
                                     active === link.label
                                         ? 'underline underline-offset-4 decoration-2'
                                         : ''
