040c9be84d7da1856c02b1e72e5ab3bba740ee28
Drew Larson <116033515+drewlarso@users.noreply.github.com>
2025-08-04 01:42:35 -0600

    finish hero section and start skills section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,20 +22,20 @@ export default function Hero() {
                 <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-center mb-6">
                     Drew Larson
                 </h1>
-                <h1 className="text-md sm:text-xl md:text-2xl mt-6 font-medium loaded mb-6">
+                <h1 className="text-md sm:text-xl md:text-2xl mt-4 font-medium loaded mb-8">
                     Software Engineering Student
                 </h1>
                 <div className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-24 h-0.5 bg-gradient-to-r from-transparent via-violet-400 to-transparent"></div>
             </div>
             <h1
-                className={`text-md sm:text-lg md:text-xl text-center text-violet-200 mt-6 font-medium min-w-[50%] w-xl transform transition-all duration-1000 delay-300 ${
+                className={`text-sm sm:text-base md:text-lg lg:text-xl text-center text-violet-200 mt-8 font-medium max-w-2xl mx-auto px-4 leading-relaxed transform transition-all duration-1000 delay-300 ${
                     isLoaded
                         ? 'translate-y-0 opacity-100'
                         : 'translate-y-10 opacity-0'
                 }`}
             >
-                hellohellohellohellohellohellohellohello libgy is chump
-                llohellohellohellohellohello hellohellohellohellohellohellohello
+                Building the bridge between imagination and creation. I'm
+                passionate about making software that people love to use.
             </h1>
             <div
                 className={`flex flex-row justify-center items-center gap-4  transform transition-all duration-1000 delay-500 ${
@@ -44,15 +44,22 @@ export default function Hero() {
                         : 'translate-y-5 opacity-0'
                 }`}
             >
-                <button className="mt-16 font-medium bg-gradient-to-br from-violet-700/80 to-indigo-600/80 hover:from-violet-600/80 hover:to-indigo-500/80 transition-all duration-200 border-2 border-violet-600/20 backdrop-blur-sm py-2 px-3 rounded-4xl overflow-hidden hover:scale-105">
-                    <a href="#projects">My Projects</a>
+                <button className="mt-16 font-mediumtransition-all duration-200 backdrop-blur-sm p-1 rounded-xl overflow-hidden hover:scale-110">
+                    <a href="https://www.github.com/drewlarso" target="_blank">
+                        <img src="github.svg" width={42}></img>
+                    </a>
                 </button>
-                <button className="mt-16 font-medium bg-gradient-to-br from-violet-700/80 to-indigo-600/80 hover:from-violet-600/80 hover:to-indigo-500/80 transition-all duration-200 border-2 border-violet-600/20 backdrop-blur-sm py-2 px-3 rounded-4xl overflow-hidden hover:scale-105">
-                    <a href="#contact">Contact Me</a>
+                <button className="mt-16 font-mediumtransition-all duration-200 backdrop-blur-sm p-1 rounded-xl overflow-hidden hover:scale-110">
+                    <a
+                        href="https://www.linkedin.com/in/drew-larson-329746325/"
+                        target="_blank"
+                    >
+                        <img src="linkedin.svg" width={48}></img>
+                    </a>
                 </button>
             </div>
             <p
-                className={`absolute bottom-24 font-bold text-3xl mt-16 transform transition-all duration-1000 delay-500 animate-bounce ${
+                className={`absolute bottom-32 font-bold text-3xl mt-8 transform transition-all duration-1000 delay-500 animate-bounce ${
                     isLoaded
                         ? 'translate-y-0 opacity-100'
                         : 'translate-y-5 opacity-0'
